b63022347f65881e41c2a50e45adc939f5f70b31
MarkWolf257 <sambhunathmeher@gmail.com>
2023-06-21 09:57:27 +0530

    Getting ready for seed

---
+++ infrastructure/test/DynamoDBStack.test.js
@@ -0,0 +1,16 @@
+import { Template } from "aws-cdk-lib/assertions";
+import * as sst from "@serverless-stack/resources";
+import DynamoDBStack from "../lib/DynamoDBStack";
+
+test("Test Stack", () => {
+  const app = new sst.App();
+  // WHEN
+  const stack = new DynamoDBStack(app, "test-stack");
+  // THEN
+  const template = Template.fromStack(stack);
+  template.hasResource("AWS::DynamoDB::Table", {
+    Properties: {
+      BillingMode: "PAY_PER_REQUEST",
+    },
+  });
+});
