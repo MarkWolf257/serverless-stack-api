47b0b89887945989c8b7e50ea72d1bbd7c54a914
MarkWolf257 <sambhunathmeher@gmail.com>
2023-06-22 20:33:40 +0530

    Adding error logging

--- services/notes/libs/handler-lib.js
+++ services/notes/libs/handler-lib.js
@@ -1,11 +1,17 @@
+import * as debug from "./debug-lib";
+
 export default function handler(lambda) {
     return async function (event, context) {
         let body, statusCode;
+
+        debug.init(event, context);
 
         try {
             body = await lambda(event, context);
             statusCode = 200;
         } catch (e) {
+            debug.flush(e);
+
             body = {error: e.message};
             statusCode = 500;
         }
@@ -16,7 +22,7 @@ export default function handler(lambda) {
             headers: {
                 "Access-Control-Allow-Origin": "*",
                 "Access-Control-Allow-Credentials": true,
-            }
+            },
         };
     };
 }
